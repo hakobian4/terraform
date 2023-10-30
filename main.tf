3a61c6c8f4653ba3f214d5842b9ff51419d129f3
hakobian4 <armenhakobyan781@gmail.com>
2023-10-30 12:16:44 +0400

    modularize terraform project

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-  region = "eu-west-3"
-}
-
-variable "vpc_cidr_blocks" {}
-variable "subnet_cidr_blocks" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
   tags       = {
@@ -17,152 +5,24 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_blocks
-  availability_zone = var.avail_zone
-  tags              = {
-    Name : "${var.env_prefix}-subnet-1"
-  }
-}
-
-#resource "aws_route_table" "myapp-route-table" {
-#  vpc_id = aws_vpc.myapp-vpc.id
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.myapp-igw.id
-#  }
-#  tags = {
-#    Name : "${var.env_prefix}-rtb"
-#  }
-#}
-#
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags   = {
-    Name : "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "default-rtb" {
+module "myapp-subnet" {
+  source                 = "./modules/subnet"
+  vpc_id                 = aws_vpc.myapp-vpc.id
+  subnet_cidr_blocks     = var.subnet_cidr_blocks
+  avail_zone             = var.avail_zone
+  env_prefix             = var.env_prefix
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name : "${var.env_prefix}-default-rtb"
-  }
 }
 
-#
-#resource "aws_route_table_association" "a-rtb-subnet" {
-#  subnet_id = aws_subnet.myapp-subnet-1.id
-#  route_table_id = aws_route_table.myapp-route-table.id
-#}
-
-#resource "aws_security_group" "myapp-sg" {
-#  name   = "myapp-sg"
-#  vpc_id = aws_vpc.myapp-vpc.id
-#
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = [var.my_ip]
-#  }
-#
-#  ingress {
-#    from_port   = 8080
-#    to_port     = 8080
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port = "0"
-#    to_port = "0"
-#    protocol = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#    prefix_list_ids = []
-#  }
-#
-#  tags = {
-#    Name: "${var.env_prefix}-sg"
-#  }
-#}
-
-resource "aws_default_security_group" "default-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-
-  tags = {
-    Name: "${var.env_prefix}-default-sg"
-  }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners = ["137112412989"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name = "myapp-server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh")
-
-  tags = {
-    Name: "${var.env_prefix}-server"
-  }
+module "myapp-server" {
+  source              = "./modules/webserver"
+  vpc_id              = aws_vpc.myapp-vpc.id
+  my_ip               = var.my_ip
+  env_prefix          = var.env_prefix
+  image_name          = var.image_name
+  public_key_location = var.public_key_location
+  instance_type       = var.instance_type
+  subnet_id           = module.myapp-subnet.subnet.id
+  avail_zone          = var.avail_zone
 }
 
