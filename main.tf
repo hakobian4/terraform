22e87b79acfbc4ab4fc3c519d24227a45e0841f8
hakobian4 <armenhakobyan781@gmail.com>
2024-01-23 16:00:04 +0400

    removed unnecessary files and added the latest changes

--- main.tf
+++ main.tf
@@ -26,17 +26,6 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 
-#resource "aws_route_table" "myapp-route-table" {
-#  vpc_id = aws_vpc.myapp-vpc.id
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.myapp-igw.id
-#  }
-#  tags = {
-#    Name : "${var.env_prefix}-rtb"
-#  }
-#}
-#
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags   = {
@@ -54,43 +43,6 @@ resource "aws_default_route_table" "default-rtb" {
     Name : "${var.env_prefix}-default-rtb"
   }
 }
-
-#
-#resource "aws_route_table_association" "a-rtb-subnet" {
-#  subnet_id = aws_subnet.myapp-subnet-1.id
-#  route_table_id = aws_route_table.myapp-route-table.id
-#}
-
-#resource "aws_security_group" "myapp-sg" {
-#  name   = "myapp-sg"
-#  vpc_id = aws_vpc.myapp-vpc.id
-#
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = [var.my_ip]
-#  }
-#
-#  ingress {
-#    from_port   = 8080
-#    to_port     = 8080
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port = "0"
-#    to_port = "0"
-#    protocol = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#    prefix_list_ids = []
-#  }
-#
-#  tags = {
-#    Name: "${var.env_prefix}-sg"
-#  }
-#}
 
 resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
@@ -165,4 +117,3 @@ resource "aws_instance" "myapp-server" {
     Name: "${var.env_prefix}-server"
   }
 }
-
